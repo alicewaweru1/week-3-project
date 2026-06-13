31a17675d63d251dc1a544c38d77a29cff7f38e6
alice-waweru <alicewaweru003@5676>
2026-06-13 13:00:54 +0300

    add loops; extract book titles; extract book price

--- bookscrape.py
+++ bookscrape.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = 'http://books.toscrape.com/index.html'
 
@@ -7,3 +8,21 @@ response = requests.get(url)
 print(response.status_code)
 
 #print(response.content)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+cards = soup.select('.product_pod')
+print(cards[0])
+all_the_books = []
+for card in cards:
+    title = card.select_one('h3 a').text
+    print(title)
+    price = card.select_one('.price_color').text
+    print(price)
+    
+    book_item = {
+        'title': title,
+        'price': price
+    }
+    all_the_books.append(book_item)
+print(all_the_books)
